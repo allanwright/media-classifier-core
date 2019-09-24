5e169e6ee611a7c754e9ed8752bf835077faaa06
Allan Wright <allan.s.wright@gmail.com>
2019-09-24 10:41:05 +0000

    Add package dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='mccore',
-    version='0.0.7',
+    version='0.0.8',
     author='Allan Wright',
     description='media-classifier-core package',
     long_description=long_description,
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
+    install_requires=[
+        'numpy',
+        'sklearn'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
