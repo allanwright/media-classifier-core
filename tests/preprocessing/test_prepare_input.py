c1553ae0a6dc132301b054db5f07c294a05b201b
Allan Wright <allan.s.wright@gmail.com>
2019-11-07 20:37:24 +0000

    Refactor unit tests

--- tests/preprocessing/test_prepare_input.py
+++ tests/preprocessing/test_prepare_input.py
@@ -1,25 +1,20 @@
+import pytest
 from mccore import preprocessing
 
-def test_prepare_input_removes_commas():
-    assert preprocessing.prepare_input('a,a,a') == 'aaa'
-
-def test_prepare_input_removes_leading_commas():
-    assert preprocessing.prepare_input(',,a') == 'a'
-
-def test_prepare_input_removes_trailing_commas():
-    assert preprocessing.prepare_input('a,,') == 'a'
+@pytest.mark.parametrize('input, expected',
+    [('a,a,a', 'aaa'), (',,a', 'a'), ('a,,', 'a')])
+def test_prepare_input_removes_commas(input, expected):
+    assert preprocessing.prepare_input(input) == expected
 
 def test_prepare_input_outputs_lower():
     assert preprocessing.prepare_input('AbCd') == 'abcd'
 
-def test_prepare_input_removes_path():
-    assert preprocessing.prepare_input('dir/file.ext') == 'file ext'
-
-def test_prepare_input_removes_path_leading_slash():
-    assert preprocessing.prepare_input('/dir/file.ext') == 'file ext'
-
-def test_prepare_input_removes_deep_path():
-    assert preprocessing.prepare_input('dir1/dir2/file.ext') == 'file ext'
+@pytest.mark.parametrize('input, expected',
+    [('dir/file.ext', 'file ext'),
+    ('/dir/file.ext', 'file ext'),
+    ('dir1/dir2/file.ext', 'file ext')])
+def test_prepare_input_removes_path(input, expected):
+    assert preprocessing.prepare_input(input) == expected
 
 def test_prepare_input_normalizes_word_separators():
     assert preprocessing.prepare_input('a.b_c-d[e]f+g') == 'a b c d e f g'
@@ -31,17 +26,13 @@ def test_prepare_input_removes_punctuation():
 def test_prepare_input_splits_season_episode():
     assert preprocessing.prepare_input('s01e01') == 's01 e01'
 
-def test_prepare_input_removes_leading_spaces():
-    assert preprocessing.prepare_input(' file.ext') == 'file ext'
-
-def test_prepare_input_removes_trailing_spaces():
-    assert preprocessing.prepare_input('file.ext ') == 'file ext'
-
-def test_prepare_input_removes_duplicate_spaces():
-    assert preprocessing.prepare_input('file  ext') == 'file ext'
-
-def test_prepare_input_retains_single_spaces():
-    assert preprocessing.prepare_input('file ext') == 'file ext'
+@pytest.mark.parametrize('input, expected',
+    [(' file.ext', 'file ext'),
+    ('file.ext ', 'file ext'),
+    ('file  ext', 'file ext'),
+    ('file ext', 'file ext')])
+def test_prepare_input_removes_extraneous_spaces(input, expected):
+    assert preprocessing.prepare_input(input) == expected
 
 def test_prepare_input_movie():
     assert preprocessing.prepare_input(
