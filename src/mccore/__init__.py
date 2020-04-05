05c1b3cbc27f5e480691b9db4dcc4e54d837f2be
Allan Wright <allan.s.wright@gmail.com>
2020-04-05 03:21:31 +0000

    Make importing classes easier

--- src/mccore/__init__.py
+++ src/mccore/__init__.py
@@ -3,7 +3,8 @@ Machine learning project for filename based media classification and named
 entity recognition.
 '''
 
-import mccore.classifier
-import mccore.entity_recognizer
+import mccore.classifier as classifier
+import mccore.entity_recognizer as entity_recognizer
 
-name = 'mccore'
+Classifier = classifier.Classifier
+EntityRecognizer = entity_recognizer.EntityRecognizer
