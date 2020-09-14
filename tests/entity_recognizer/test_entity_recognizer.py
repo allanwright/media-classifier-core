4bea396ca37d65ffb00d89266b53f13dc44ea982
Allan Wright <allan.s.wright@gmail.com>
2020-09-14 08:09:41 +0000

    Fix linting issues

--- tests/entity_recognizer/test_entity_recognizer.py
+++ tests/entity_recognizer/test_entity_recognizer.py
@@ -5,16 +5,16 @@ import pytest
 
 from mccore.entity_recognizer import EntityRecognizer
 
-@pytest.fixture
-def model():
+@pytest.fixture(name='model')
+def fixture_model():
     return EntityRecognizer.load_default()
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('Billy Bonka & the Milk Factory (1971) (1080p RedRay x275 16bit Something).mkv',
       'Billy Bonka and the Milk Factory'),
      ('Don\'t.Tell.Dad.The.Garden\'s.Dead.1991.1080p.WEBCut.x275-[YBS.LX].mp4',
       'Don\'t Tell Dad the Garden\'s Dead')])
-def test_predict_produces_correct_title(model, input, expected):
-    entities = dict(model.predict(input))
+def test_predict_produces_correct_title(model, name, expected):
+    entities = dict(model.predict(name))
     assert entities['title'] == expected
