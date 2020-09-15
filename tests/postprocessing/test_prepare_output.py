68edf194fc225b839a8b49f42006d4cc8c6d28ee
Allan Wright <allan.s.wright@gmail.com>
2020-09-15 05:51:02 +0000

    Fix linting issues

--- tests/postprocessing/test_prepare_output.py
+++ tests/postprocessing/test_prepare_output.py
@@ -1,20 +1,27 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 import pytest
 from mccore import postprocessing
 
-@pytest.mark.parametrize('input, expected',
-    [('a,a,a', 'aaa'), (',,a', 'a'), ('a,,', 'a')])
-def test_prepare_output_removes_commas(input, expected):
-    assert postprocessing.prepare_output(input) == expected
+@pytest.mark.parametrize(
+    'name, expected',
+    [('a,a,a', 'aaa'),
+     (',,a', 'a'),
+     ('a,,', 'a')])
+def test_prepare_output_removes_commas(name, expected):
+    assert postprocessing.prepare_output(name) == expected
 
 def test_prepare_output_outputs_retains_case():
     assert postprocessing.prepare_output('AbCd') == 'AbCd'
 
-@pytest.mark.parametrize('input, expected',
+@pytest.mark.parametrize(
+    'name, expected',
     [('dir/file.ext', 'file ext'),
-    ('/dir/file.ext', 'file ext'),
-    ('dir1/dir2/file.ext', 'file ext')])
-def test_prepare_output_removes_path(input, expected):
-    assert postprocessing.prepare_output(input) == expected
+     ('/dir/file.ext', 'file ext'),
+     ('dir1/dir2/file.ext', 'file ext')])
+def test_prepare_output_removes_path(name, expected):
+    assert postprocessing.prepare_output(name) == expected
 
 def test_prepare_output_normalizes_word_separators():
     assert postprocessing.prepare_output('a.b_c-d[e]f+g') == 'a b c d e f g'
@@ -30,21 +37,22 @@ def test_prepare_output_retains_punctuation():
 def test_prepare_output_splits_season_episode():
     assert postprocessing.prepare_output('s01e01') == 's01 e01'
 
-@pytest.mark.parametrize('input, expected',
+@pytest.mark.parametrize(
+    'name, expected',
     [(' file.ext', 'file ext'),
-    ('file.ext ', 'file ext'),
-    ('file  ext', 'file ext'),
-    ('file ext', 'file ext')])
-def test_prepare_output_removes_extraneous_spaces(input, expected):
-    assert postprocessing.prepare_output(input) == expected
+     ('file.ext ', 'file ext'),
+     ('file  ext', 'file ext'),
+     ('file ext', 'file ext')])
+def test_prepare_output_removes_extraneous_spaces(name, expected):
+    assert postprocessing.prepare_output(name) == expected
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('Me & u', 'Me and u'),
-    ('Me&u', 'Me and u'),
-    ('Me&', 'Me and'),
-    ('&u', 'and u')])
-def test_prepare_output_converts_ampersand_to_and(input, expected):
+     ('Me&u', 'Me and u'),
+     ('Me&', 'Me and'),
+     ('&u', 'and u')])
+def test_prepare_output_converts_ampersand_to_and(name, expected):
     assert postprocessing.prepare_output(input) == expected
 
 def test_prepare_output_movie():
