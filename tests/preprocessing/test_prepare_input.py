8db3843e00fa8f08bfbca0d331b01a9af2fee2e6
Allan Wright <allan.s.wright@gmail.com>
2020-09-17 08:05:45 +0000

    Fix linting issues

--- tests/preprocessing/test_prepare_input.py
+++ tests/preprocessing/test_prepare_input.py
@@ -1,22 +1,27 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 import pytest
 from mccore import preprocessing
 
 @pytest.mark.parametrize(
-    'input, expected',
-    [('a,a,a', 'aaa'), (',,a', 'a'), ('a,,', 'a')])
-def test_prepare_input_removes_commas(input, expected):
-    assert preprocessing.prepare_input(input) == expected
+    'name, expected',
+    [('a,a,a', 'aaa'),
+     (',,a', 'a'),
+     ('a,,', 'a')])
+def test_prepare_input_removes_commas(name, expected):
+    assert preprocessing.prepare_input(name) == expected
 
 def test_prepare_input_outputs_lower():
     assert preprocessing.prepare_input('AbCd') == 'abcd'
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('dir/file.ext', 'file ext'),
-    ('/dir/file.ext', 'file ext'),
-    ('dir1/dir2/file.ext', 'file ext')])
-def test_prepare_input_removes_path(input, expected):
-    assert preprocessing.prepare_input(input) == expected
+     ('/dir/file.ext', 'file ext'),
+     ('dir1/dir2/file.ext', 'file ext')])
+def test_prepare_input_removes_path(name, expected):
+    assert preprocessing.prepare_input(name) == expected
 
 def test_prepare_input_normalizes_word_separators():
     assert preprocessing.prepare_input('a.b_c-d[e]f+g') == 'a b c d e f g'
@@ -29,22 +34,22 @@ def test_prepare_input_splits_season_episode():
     assert preprocessing.prepare_input('s01e01') == 's01 e01'
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [(' file.ext', 'file ext'),
-    ('file.ext ', 'file ext'),
-    ('file  ext', 'file ext'),
-    ('file ext', 'file ext')])
-def test_prepare_input_removes_extraneous_spaces(input, expected):
-    assert preprocessing.prepare_input(input) == expected
+     ('file.ext ', 'file ext'),
+     ('file  ext', 'file ext'),
+     ('file ext', 'file ext')])
+def test_prepare_input_removes_extraneous_spaces(name, expected):
+    assert preprocessing.prepare_input(name) == expected
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('Me & u', 'me and u'),
-    ('Me&u', 'me and u'),
-    ('Me&', 'me and'),
-    ('&u', 'and u')])
-def test_prepare_input_converts_ampersand_to_and(input, expected):
-    assert preprocessing.prepare_input(input) == expected
+     ('Me&u', 'me and u'),
+     ('Me&', 'me and'),
+     ('&u', 'and u')])
+def test_prepare_input_converts_ampersand_to_and(name, expected):
+    assert preprocessing.prepare_input(name) == expected
 
 def test_prepare_input_movie():
     assert preprocessing.prepare_input(
--- tests/preprocessing/test_split_season_episode.py
+++ tests/preprocessing/test_split_season_episode.py
@@ -1,3 +1,6 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 import pytest
 from mccore import preprocessing
 
@@ -7,26 +10,27 @@ def test_split_season_episode_empty():
 def test_split_season_episode_invalid():
     assert preprocessing.split_season_episode('1234') == '1234'
 
-@pytest.mark.parametrize('input, expected',
+@pytest.mark.parametrize(
+    'name, expected',
     [('s01e01', 's01 e01'),
-    ('S01E01', 'S01 E01'),
-    ('S01e01', 'S01 e01'),
-    ('s9999e9999', 's9999 e9999')])
-def test_split_season_episode_standard_format(input, expected):
-    assert preprocessing.split_season_episode(input) == expected
+     ('S01E01', 'S01 E01'),
+     ('S01e01', 'S01 e01'),
+     ('s9999e9999', 's9999 e9999')])
+def test_split_season_episode_standard_format(name, expected):
+    assert preprocessing.split_season_episode(name) == expected
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('01x01', 's01 e01'),
      ('01X01', 's01 e01'),
      ('9999x9999', 's9999 e9999')])
-def test_split_season_episode_x_format(input, expected):
-    assert preprocessing.split_season_episode(input) == expected
+def test_split_season_episode_x_format(name, expected):
+    assert preprocessing.split_season_episode(name) == expected
 
 @pytest.mark.parametrize(
-    'input, expected',
+    'name, expected',
     [('s01 ep01', 's01 e01'),
      ('S01 EP01', 's01 e01'),
      ('S9999 Ep9999', 's9999 e9999')])
-def test_split_season_episode_ep_format(input, expected):
-    assert preprocessing.split_season_episode(input) == expected
+def test_split_season_episode_ep_format(name, expected):
+    assert preprocessing.split_season_episode(name) == expected
